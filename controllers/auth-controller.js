b91d922cf403c671ee3712608eb5da775ed33d5a
AndreyTarkaniy <endrubass@gmail.com>
2023-07-26 19:10:28 +0300

    add hash-password option

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,14 +1,20 @@
+import bcrypt from "bcryptjs";
+
 import User from "../models/userModel.js";
 import { controlWrapper } from "../decorators/index.js";
 import { HttpError } from "../helpers/index.js";
 
 const authSignup = async (req, res) => {
-  const newUser = await User.create(req.body);
+  const { password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: passwordHash });
 
   res.status(201).json({
     name: newUser.name,
     email: newUser.email,
   });
+  //   console.log(req.body);
 };
 
 export default {
